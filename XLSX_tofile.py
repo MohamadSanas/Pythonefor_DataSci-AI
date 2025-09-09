9731e30ab57609e9ed334d7ec1650f738063cc93
Mohamad Sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-09-09 16:40:35 +0530

    explored that, how to access json filewith python

---
+++ XLSX_tofile.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests
+
+# URL of the Excel file
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/file_example_XLSX_10.xlsx"
+
+# Download the file
+response = requests.get(url)
+with open("file_example_XLSX_10.xlsx", "wb") as f:
+    f.write(response.content)
+
+# Read the Excel file into a DataFrame
+df = pd.read_excel("file_example_XLSX_10.xlsx")
+
+print(df)
