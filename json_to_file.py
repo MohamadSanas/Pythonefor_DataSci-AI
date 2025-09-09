ec90c386b8e9a21ff5383fdf0f125157b0a21b94
Mohamad Sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-09-09 16:40:35 +0530

    explored that, how to access json filewith python

---
+++ json_to_file.py
@@ -0,0 +1,27 @@
+import json
+person = {
+    'first_name' : 'Mark',
+    'last_name' : 'abc',
+    'age' : 27,
+    'address': {
+        "streetAddress": "21 2nd Street",
+        "city": "New York",
+        "state": "NY",
+        "postalCode": "10021-3100"
+    }
+}
+
+# this was a writing JSON to a File
+with open ("person.json","w") as f:
+    json.dump(person,f)
+    
+json_object = json.dumps(person, indent = 4) 
+
+with open("sample.json", "w") as outfile: 
+    outfile.write(json_object) 
+    
+    
+#Reading JSON to a File
+with open("sample.json","r") as openfile:
+    json_read=json.load(openfile)
+print(json_read)
